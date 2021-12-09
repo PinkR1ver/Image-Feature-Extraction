018f4d9d668958ef06cb5ace19965a2884631411
PinkR1ver <3180102330@zju.edu.cn>
2021-12-09 15:53:29 +0800

    Update PIL Image Read Image situation and can be deployed both in '1' mode or 'L' mode

--- IntensityBasedFeatures.py
+++ IntensityBasedFeatures.py
@@ -11,16 +11,19 @@ def mean_of_image(image, masks=np.array([])):
         im.shape = (w*h, d)
         return tuple(np.average(im, axis=0))
     else:
-        sum=[0 ,0 ,0]
+        sum = np.array([0 ,0 ,0])
         iter = 0
         for i in range(w):
             for j in range(h):
-                if masks[i, j] == 1:
+                if masks[i, j] == 1 or 255:
                     sum +=im[i,j]
                     iter +=1
         sum = sum.astype('float32')
-        sum /= iter
-        return tuple(sum)
+        if sum.all() != 0:
+            sum /= iter
+            return tuple(sum)
+        else:
+            return tuple([0, 0, 0])
 
 def variance_of_image(image, masks=np.array([])):
     im = np.array(image)
@@ -29,11 +32,11 @@ def variance_of_image(image, masks=np.array([])):
         im.shape = (w*h, d)
         return tuple(np.var(im, axis=0))
     else:
-        sum=[0 ,0 ,0]
+        sum = np.array([0 ,0 ,0])
         iter = 0
         for i in range(w):
             for j in range(h):
-                if masks[i, j] == 1:
+                if masks[i, j] == 1 or 255:
                     sum += im[i, j]
                     iter +=1
         sum = sum.astype('float32')
@@ -46,11 +49,15 @@ def variance_of_image(image, masks=np.array([])):
         sum /= iter
         return tuple(sum)
 
+def skewness_of_image(image, masks=np.array([])):
+    pass
+
 
 if __name__=='__main__':
     rocket = data.rocket()
     grayscale = color.rgb2gray(rocket)
-    print(grayscale.shape)
+    io.imshow(grayscale)
+    plt.show()
     for i in range(grayscale.shape[0]):
         for j in range(grayscale.shape[1]):
             if grayscale[i,j] > 0.5:
@@ -65,7 +72,23 @@ if __name__=='__main__':
     plt.show()
     io.imshow(grayscale)
     plt.show()
+    print(grayscale.shape)
     white = np.ones(grayscale.shape)
     io.imshow(white)
     plt.show()
 
+    Brain = Image.open(r'sample/TCGA_CS_4941_19960909_18.tif')
+    Brain_mask = Image.open(r'sample/TCGA_CS_4941_19960909_18_mask.tif')
+    #Brain_mask = Brain_mask.convert('1')
+    print(Brain_mask.mode)
+    Brain_mask = np.array(Brain_mask)
+    Brain = np.array(Brain)
+    print(Brain_mask.shape)
+    io.imshow(Brain_mask)
+    plt.show()
+    print(Brain.shape)
+    io.imshow(Brain)
+    plt.show()
+    #print(np.nonzero(Brain_mask))
+    Brain_mean = mean_of_image(Brain, Brain_mask)
+    print(Brain_mean)
